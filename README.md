2308a8bca32cdf3bcf0403ed3a484048f734a72d
luoyt <501244162@qq.com>
2019-04-03 15:30:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# flutter app 练习
+
+[正式版下载](app-release.apk)
+
+A new Flutter application.
+
+## 规范
+
+[代码开发规范](代码开发规范.md)
+
+## 教程
+
+https://flutterchina.club/widgets-intro/
+
+https://book.flutterchina.club/
+
+
+
+## 搜索库
+
+https://pub.dartlang.org/flutter/packages?q=
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: new AppBar(title: Text("首页"),),
+        body: Text("破竹App"),
+      ),
+    );
+  }
+}
